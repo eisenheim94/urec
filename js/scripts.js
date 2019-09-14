7ece3b304cb8d5a321a180a3521e92277759b045
Demyd Ganenko <ganenko94@gmail.com>
2019-09-14 11:19:20 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// $('#fullpage').fullpage({
+	// 	// navigation: true,
+	// 	// navigationPosition: 'right',
+	// 	// navigationTooltips: ['First page', 'Second page', 'Third and last page']
+	// });
+	$('.slick-benefits').slick({
+		prevArrow: $('#benefits .slick-prev'),
+		nextArrow: $('#benefits .slick-next'),
+	});
+});
